c178d0d932bf0f5dc2fda99b29f8ac8f168d7138
Shinsuke Nishio <nishio@densan-labs.net>
2014-12-25 01:04:50 +0900

    allow foreign key reference symbol definition

--- lib/convergence/foreign_key.rb
+++ lib/convergence/foreign_key.rb
@@ -4,7 +4,7 @@ class Convergence::ForeignKey
   def initialize(key_name, from_columns, to_table, to_columns, options)
     @key_name = key_name
     @from_columns = [from_columns].flatten.map(&:to_s)
-    @to_table = to_table
+    @to_table = to_table.to_s
     @to_columns = [to_columns].flatten.map(&:to_s)
     @options = { name: @key_name }.merge(options)
   end
