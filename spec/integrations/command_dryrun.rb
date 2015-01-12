7c76f83d39f3f3c2b73b263ef249e591957386c0
Shinsuke Nishio <nishio@densan-labs.net>
2015-01-12 16:56:24 +0900

    spec: add spec_helper to integration test

--- spec/integrations/command_dryrun.rb
+++ spec/integrations/command_dryrun.rb
@@ -1,3 +1,5 @@
+require 'spec_helper'
+
 describe 'Command::Dryrun#execute' do
   def execute(dsl_path)
     parse_option = {
--- spec/integrations/drop_foreign_key.rb
+++ spec/integrations/drop_foreign_key.rb
@@ -1,3 +1,5 @@
+require 'spec_helper'
+
 describe 'Changeable Foreign key' do
   after(:each) do
     rollback
