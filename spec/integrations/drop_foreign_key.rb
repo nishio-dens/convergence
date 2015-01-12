9188cfb3e1bebd4c622948c98bb3245780e6ed5e
Shinsuke Nishio <nishio@densan-labs.net>
2015-01-12 16:54:50 +0900

    add spec about drop foreign key

---
+++ spec/integrations/drop_foreign_key.rb
@@ -0,0 +1,21 @@
+describe 'Changeable Foreign key' do
+  after(:each) do
+    rollback
+  end
+
+  def execute(dsl_path)
+    parse_option = {
+      apply: true,
+      input: File.expand_path("#{File.dirname(__FILE__)}/../fixtures/#{dsl_path}")
+    }
+    Convergence::Command.new(parse_option, config: mysql_settings).execute
+  end
+
+  describe 'drop foreign key' do
+    let(:exec_dsl) { 'drop_foreign_key.schema' }
+
+    it 'should be drop foreign key' do
+      expect { execute(exec_dsl) }.not_to raise_error
+    end
+  end
+end
