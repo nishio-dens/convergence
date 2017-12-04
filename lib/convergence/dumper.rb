22c9b7e801ebffd4e16715211a4a969312c66c11
Shinsuke Nishio <nishio@densan-labs.net>
2017-12-05 01:13:27 +0900

    export tinyint limit:1 as boolean

--- lib/convergence/dumper.rb
+++ lib/convergence/dumper.rb
@@ -28,8 +28,16 @@ class Convergence::Dumper
 
   def dump_column(column)
     argument = [%(:#{column.column_name})]
-    argument << column.options.map { |k, v| key_value_text(k, v) }
-    "t.#{column.type} #{argument.flatten.join(', ')}"
+    case [column.type, column.options[:limit]]
+    when [:tinyint, '1']
+      column_type = "boolean"
+      argument << column.options.reject { |k, _| k == :limit }.map { |k, v| key_value_text(k, v) }
+    else
+      column_type = column.type
+      argument << column.options.map { |k, v| key_value_text(k, v) }
+    end
+
+    "t.#{column_type} #{argument.flatten.join(', ')}"
   end
 
   def dump_index(index)
